ce7e38463a324da4bad3d361400a678fe6ce26a1
Fadhlan <fadhlannur007@gmail.com>
2025-02-17 23:09:34 +0700

    nambah yang no 2

---
+++ LAB DATASTRUCT/labpt1no2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int maxProfit(int harga_saham[], int ukuran) {
+    if (ukuran == 0) return 0;
+
+    int min_harga = harga_saham[0];
+    int max_profit = 0;
+
+    for (int i = 1; i < ukuran; i++) {
+        if (harga_saham[i] < min_harga) {
+            min_harga = harga_saham[i];
+        } else {
+            int profit = harga_saham[i] - min_harga;
+            if (profit > max_profit) {
+                max_profit = profit;
+            }
+        }
+    }
+
+    return max_profit;
+}
+
+int main() {
+    int ukuran;
+
+    printf("Masukkan jumlah harga saham: ");
+    scanf("%d", &ukuran);
+
+    if (ukuran <= 0) {
+        printf("Jumlah harga saham harus lebih dari 0.\n");
+        return 1;
+    }
+
+    int harga_saham[ukuran];
+
+    printf("Masukkan harga saham:\n");
+    for (int i = 0; i < ukuran; i++) {
+        scanf("%d", &harga_saham[i]);
+    }
+
+    printf("Profit maksimum: %d\n", maxProfit(harga_saham, ukuran));
+
+    return 0;
+}
